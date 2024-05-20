296d209c5bfde2bff922c66fdafecbbb5eb299b4
edisoncor <edisoncor@gmail.com>
2024-05-20 09:38:57 -0500

    Operciones

---
+++ operaciones.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int s1 = 2;
+    int s2 = 3;
+    int suma = s1 + s2;
+
+    int resta = 6-8;
+
+    int m1 = 6;
+    int multiplicacion = m1 *5;
+
+    int d1 = 0;
+    int division = d1 / 123;
+
+    printf("La suma es %d\n", suma);
+    printf("La resta es %d\n", resta);
+    printf("La multipliación es %d\n", multiplicacion);
+    printf("La división es %d\n", division);
+
+    return 0;
+}
+
+
