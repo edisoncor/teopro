7a338bb94fe2c267c2a0a3861ca67bf5404e53b8
edisoncor <edisoncor@gmail.com>
2024-05-20 09:44:31 -0500

    operaciones

--- operaciones.c
+++ operaciones.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int main(){
@@ -22,3 +23,28 @@ int main(){
 }
 
 
+=======
+
+#include <stdio.h>
+
+int main(){
+    int s1 = 2; // Declaración e inicialización
+    int s2 = 3; // Declaración e inicialización
+    int suma = s1 + s2; // Declaración e inicialización
+
+    int resta = 6-8; // Declaración e inicialización
+
+    int m1 = 6; // Declaración e inicialización
+    int multiplicacion = m1 *5; // Declaración e inicialización
+
+    int d1 = 0; // Declaración e inicialización
+    int division = d1 / 123; // Declaración e inicialización
+
+    printf("La suma es %d\n", suma); // %d para enteros
+    printf("La resta es %d\n", resta); // %d para enteros
+    printf("La multipliación es %d\n", multiplicacion); // %d para enteros
+    printf("La división es %d\n", division); // %d para enteros
+
+    return 0;
+}
+>>>>>>> v0.2.0
